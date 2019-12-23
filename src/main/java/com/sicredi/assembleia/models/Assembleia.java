b3f1fcd4d3a1db66e3a7d7c92d6def3cb963ae9f
gabrieRabelo <gabriel.almeida97@gmail.com>
2019-12-23 20:20:35 -0300

    00/ajusta nome de tabela

--- src/main/java/com/sicredi/assembleia/models/Assembleia.java
+++ src/main/java/com/sicredi/assembleia/models/Assembleia.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "assembleias")
+@Table(name = "assembleia")
 public class Assembleia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
