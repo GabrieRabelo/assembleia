9b2043555813f0da8e2c40354fc34e7bdebb5318
gabrieRabelo <gabriel.almeida97@gmail.com>
2019-12-29 13:20:51 -0300

    04/adiciona data de abertura e fechamento para pauta

--- src/main/java/com/sicredi/assembleia/models/Pauta.java
+++ src/main/java/com/sicredi/assembleia/models/Pauta.java
@@ -1,11 +1,14 @@
 package com.sicredi.assembleia.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -20,6 +23,8 @@ public class Pauta {
 
     @NotNull
     private String descricao;
+
+    private Date dtAbertura, dtFechamento;
 
     @NotNull
     @JoinColumn(name = "assembleia_id")
@@ -36,11 +41,19 @@ public class Pauta {
     public Pauta(
             @JsonProperty("titulo") String titulo,
             @JsonProperty("descricao") String descricao,
-            @JsonProperty("assembleia") Long assembleia_id
+            @JsonProperty("assembleia") Long assembleia_id,
+            @JsonProperty("dtFechamentoMinutos") Long dtFechamentoMinutos
             ) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.assembleia_id = assembleia_id;
+        this.dtAbertura = Date.from(Instant.now());
+        if(dtFechamentoMinutos > 0) {
+            this.dtFechamento = new Date(System.currentTimeMillis()+dtFechamentoMinutos*60*1000);
+        } else {
+            this.dtFechamento = new Date(System.currentTimeMillis()+60*1000);
+        }
+
     }
 
     public Long getId() { return this.id; }
@@ -52,4 +65,8 @@ public class Pauta {
     public Long getAssembleiaId() { return this.assembleia_id; }
 
     public List<Associado> getAssociados() { return this.associados; }
+
+    public Date getDtAbertura() { return this.dtAbertura; }
+
+    public Date getDtFechamento() { return this.dtFechamento; }
 }
